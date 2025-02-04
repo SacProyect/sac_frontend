9e05eea3c36ed3a199dbfeb94196aae40791bac1
SacProyect <sac1proyect@gmail.com>
2025-02-04 17:46:16 -0400

    Update apiConnection.jsx

--- src/components/utils/api/apiConnection.jsx
+++ src/components/utils/api/apiConnection.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiConnection = axios.create({
-	baseURL: "https://sac-backend-apc8.onrender.com",
+	baseURL: "https://sac-backend-r910.onrender.com",
 	headers: {
 		'Content-Type': 'application/json',
 	},
