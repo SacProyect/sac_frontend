32f232470614f5d285ad8fb7e85d496759c4e819
VictorDevCode21 <victorenrique2002@gmail.com>
2025-04-21 23:48:00 -0400

    Mobile css version finished

--- src/components/stats/PageOneStats.tsx
+++ src/components/stats/PageOneStats.tsx
@@ -74,11 +74,11 @@ const PageOneStats = ({rawStats} : {rawStats: Stat[]}) => {
         : 0;
 
     return (
-        <div className="flex flex-col items-center w-1/2 h-[50vh] bg-[#1c1c1b] py-8 text-white font-inter">
+        <div className="flex flex-col items-center w-full lg:w-[41vw] h-[50vh] bg-[#1c1c1b] pb-8 pt-8 lg:pt-4 text-white font-inter">
             {/* Title and Description */}
             <div className="mb-4 text-center">
-                <div className="w-full border border-[#b7c0cd] bg-[#292d33] rounded-md">
-                    <h1 className="font-semibold text-[24.5px] py-1 px-4 text-white font-inter whitespace-nowrap">
+                <div className="w-full lg:w-[36vw] border border-[#b7c0cd] bg-[#292d33] rounded-md">
+                    <h1 className="font-semibold text-sm lg:text-[18.5px] py-3 px-4 text-white font-inter whitespace-nowrap">
                         RENDIMIENTO GLOBAL DE CONTRIBUYENTES
                     </h1>
                 </div>
@@ -89,7 +89,7 @@ const PageOneStats = ({rawStats} : {rawStats: Stat[]}) => {
             </div>
 
             {/* Chart */}
-            <div className="w-full h-full text-xs">
+            <div className="w-full lg:w-[41vw] h-full text-xs">
                 <ResponsiveContainer width="100%" height="100%">
                     <LineChart data={rendimientoData} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                         <CartesianGrid strokeDasharray="3 3" />
@@ -110,7 +110,7 @@ const PageOneStats = ({rawStats} : {rawStats: Stat[]}) => {
             </div>
 
             {/* Summary Below Chart */}
-            <div className="flex flex-col items-center gap-4 mt-2 text-center sm:flex-row">
+            <div className="flex flex-col items-center gap-4 mt-0 text-center sm:flex-row">
                 <div className="px-4 py-2 bg-[#5996ff] rounded-lg shadow">
                     <p className="text-xs font-medium">
                         {bestMonth ? `${bestMonth.month} ha sido el mes con mejor rendimiento` : "No hay datos disponibles para determinar el mejor mes"} {/* Full name of the best month */}
