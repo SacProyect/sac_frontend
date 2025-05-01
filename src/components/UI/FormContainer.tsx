4cab8053324618a6521a13e5fab9005934ff981a
Victor <victorenrique2002@gmail.com>
2025-04-30 23:57:05 -0400

    changes in mobile version css fixed in TaxpayerForm and FormContainer

--- src/components/UI/FormContainer.tsx
+++ src/components/UI/FormContainer.tsx
@@ -5,7 +5,7 @@ import { JSX } from "react"
 function FormContainer({ children }: { children: React.ReactNode }): JSX.Element {
     return (
         <div className='flex items-center justify-center w-full h-full sm:h-screen sm:w-full lg:full lg:h-full mt-44'>
-            <div className='flex-col bg-white p-10 w-[90%] sm:w-[30rem] rounded-lg shadow-md z-10 lg:overflow-y-auto max-h-[94vh]'>
+            <div className='flex-col bg-white p-10 w-[90%] sm:w-[30rem] rounded-lg shadow-md z-10 lg:overflow-y-auto sm:max-h-full lg:max-h-[94vh]'>
                 {children}
             </div>
         </div>
