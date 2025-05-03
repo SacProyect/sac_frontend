fbc8916cab1f8d0ce932a6e15e03b35337afafc5
VictorDevCode21 <victorenrique2002@gmail.com>
2025-05-02 22:33:37 -0400

    Observations functionality implemented

--- src/components/observations/ObservationsSection.tsx
+++ src/components/observations/ObservationsSection.tsx
@@ -1,208 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TbEdit } from "react-icons/tb";
+import { getObservations } from '../utils/api/taxpayerFunctions';
+import toast from 'react-hot-toast';
 
-
-function ObservationsSection() {
-
-
-
-
-
-
-
-
-
-    return (
-        <section className='w-full h-full lg:w-[82vw] lg:h-[75vh] pb-4'>
-            <div className='flex flex-col items-center w-full h-full overflow-y-auto'>
-                {/* Sub-Title */}
-                <div className='w-2/3 h-[3rem]'>
-                    <p className='text-xl font-semibold mb-4 text-[#475569]'>Observaciones (3)</p>
-                </div>
-
-                {/* Cards */}
-                <div className='grid w-2/3 h-[10rem] sm:grid-cols-1 lg:grid-cols-2 gap-x-2 gap-y-2'>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-full h-full border border-gray-200 rounded-md shadow-md'>
-                        <div className='w-full h-full px-4 pt-4'>
-                            <p className='text-xs'>Reunion con equipo de desarrollo Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi provident porro ab in qui blanditiis expedita accusantium voluptates deserunt laborum, impedit enim aperiam quaerat aspernatur atque! Nesciunt accusamus temporibus dignissimos.</p>
-                            <div className='pt-2 pb-2'>
-                                <p className='text-xs text-gray-600'>Fecha: 2023-05-02</p>
-                            </div>
-                            <div className='flex items-center justify-end pt-2 space-x-2 border-t-2 border-gray-100'>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-gray-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Modificar</button>
-                                </div>
-                                <div className='flex items-center justify-center h-[2rem] px-1 py-0 mb-2 text-red-500 border border-gray-200 rounded-md'>
-                                    <div className='px-2'>
-                                        <TbEdit size={15} />
-                                    </div>
-                                    <button className='pl-0'>Eliminar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </section>
-    )
+interface Observation {
+    id: string;
+    description: string;
+    date: string;
 }
 
-export default ObservationsSection
+interface ObservationsSectionProps {
+    taxpayerId: string | undefined;
+}
+
+function ObservationsSection({ taxpayerId }: ObservationsSectionProps) {
+    const [observations, setObservations] = useState<Observation[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (!taxpayerId) return;
+
+        const fetchObservations = async () => {
+            try {
+                setLoading(true);
+                const response = await getObservations(taxpayerId);
+                setObservations(response); // assume this returns Observation[]
+            } catch (error) {
+                toast.error("No se pudieron obtener las observaciones de este contribuyente");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchObservations();
+    }, [taxpayerId]);
+
+    return (
+        <section className="w-full h-full lg:w-[82vw] lg:h-[75vh] pb-4">
+            <div className="flex flex-col items-center w-full h-full px-4 overflow-y-auto sm:px-6 md:px-8">
+
+                {/* Sub-Title */}
+                <div className="w-full max-w-3xl">
+                    <p className="text-lg sm:text-xl font-semibold mb-4 text-[#475569]">
+                        Observaciones ({observations.length})
+                    </p>
+                </div>
+
+                {/* Loader / Empty State */}
+                {loading ? (
+                    <p className="text-sm text-gray-500">Cargando observaciones...</p>
+                ) : observations.length === 0 ? (
+                    <p className="text-sm text-gray-500">No hay observaciones registradas.</p>
+                ) : (
+                    <div className="grid w-full max-w-3xl grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2">
+                        {observations.map((obs) => (
+                            <div
+                                key={obs.id}
+                                className="w-full border border-gray-200 rounded-md shadow-md"
+                            >
+                                <div className="w-full h-full px-4 pt-4 pb-2">
+                                    <div className="min-h-[5rem] lg:h-[70%]">
+                                        <p className="text-sm text-gray-700">{obs.description}</p>
+                                        <div className="pt-2 pb-2">
+                                            <p className="text-xs text-gray-600">Fecha: {obs.date.slice(0,10)}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex flex-wrap items-center justify-end gap-2 pt-2 border-t-2 border-gray-100 lg:h-[30%]">
+                                        <div className="flex items-center h-[2rem] px-2 text-gray-500 border border-gray-200 rounded-md">
+                                            <TbEdit size={15} />
+                                            <button className="pl-1 text-xs sm:text-sm">Modificar</button>
+                                        </div>
+                                        <div className="flex items-center h-[2rem] px-2 text-red-500 border border-gray-200 rounded-md">
+                                            <TbEdit size={15} />
+                                            <button className="pl-1 text-xs sm:text-sm">Eliminar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
+
+    );
+}
+
+export default ObservationsSection;
