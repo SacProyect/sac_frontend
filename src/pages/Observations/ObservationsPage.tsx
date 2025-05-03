8a1d470c1629b90a67e3ce11f5c4780dbaa91694
VictorDevCode21 <victorenrique2002@gmail.com>
2025-05-03 01:51:37 -0400

    functionality to edit observations implemented

--- src/pages/Observations/ObservationsPage.tsx
+++ src/pages/Observations/ObservationsPage.tsx
@@ -1,19 +1,24 @@
 import ObservationsHeader from '@/components/observations/ObservationsHeader'
 import ObservationsSection from '@/components/observations/ObservationsSection'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function ObservationsPage() {
     const { taxpayerId } = useParams()
+    const [refreshKey, setRefreshKey] = useState(0); // this key will trigger re-fetch
 
+    const handleObservationCreated = () => {
+        setRefreshKey(prev => prev + 1); // increment to force update
+        console.log("Se activa")
+    }
     
 
 
 
     return (
         <div className='bg-white'>
-            <ObservationsHeader taxpayerId={taxpayerId}/>
-            <ObservationsSection taxpayerId={taxpayerId}/>
+            <ObservationsHeader taxpayerId={taxpayerId} onObservationCreated={handleObservationCreated} />
+            <ObservationsSection taxpayerId={taxpayerId} refreshKey={refreshKey} />
         </div>
     )
 }
