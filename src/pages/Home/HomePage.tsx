6610ffdf5ac127f4474f0c4561538f073c3e4a88
VictorDevCode21 <victorenrique2002@gmail.com>
2025-05-19 14:16:10 -0400

    refresh user added in homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -12,7 +12,7 @@ import { useNavigate } from 'react-router-dom';
 import { contract_type } from '@/types/taxpayer';
 
 function HomePage() {
-    const { user } = useAuth();
+    const { user, refreshUser } = useAuth();
     const navigate = useNavigate();
 
 
@@ -22,6 +22,10 @@ function HomePage() {
         )
         return null;
     }
+
+    useEffect(() => {
+        refreshUser();
+    }, [])
 
     const [taxpayers, setTaxpayers] = useState(user?.taxpayer || [])
 
