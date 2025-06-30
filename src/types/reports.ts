f610318f326775551a0d5982a913b0cbf8fe5381
VictorDevCode21 <victorenrique2002@gmail.com>
2025-06-30 02:58:21 -0400

    FiscalInfo correctly implemented

---
+++ src/types/reports.ts
@@ -0,0 +1,9 @@
+
+export type FiscalInfo = {
+    fiscalName: string;
+    fiscalId: string;
+    totalTaxpayers: number;
+    totalProcess: number;
+    totalCompleted: number;
+    totalNotified: number;
+}
