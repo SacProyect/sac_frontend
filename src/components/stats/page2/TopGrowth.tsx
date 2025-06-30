4716a941d92a39acd4b43eeb828809ff581be4a1
VictorDevCode21 <victorenrique2002@gmail.com>
2025-06-30 12:16:53 -0400

    hotfix css

--- src/components/stats/page2/TopGrowth.tsx
+++ src/components/stats/page2/TopGrowth.tsx
@@ -142,7 +142,7 @@ function TopGrowth() {
     return (
         <>
             <div className="bg-[#2a2a29] border-[#3a3a39] rounded-md text-white">
-                <div className="flex flex-row items-center justify-between pb-4 pr-4">
+                <div className="flex flex-row items-center justify-between pb-4 lg:pb-0 pr-4">
                     <div className="flex items-center justify-center gap-2 text-lg font-semibold pt-4 pl-4">
                         <TrendingUp className="w-5 h-5 text-green-500" />
                         Coordinadores - Crecimiento Mensual
