8a8a3a3371d8720ededefc4ff194dc185eceb452
VictorDevCode21 <victorenrique2002@gmail.com>
2025-08-04 00:49:55 -0400

    trying to fix prod build

--- src/components/router/AppRouter.tsx
+++ src/components/router/AppRouter.tsx
@@ -1,7 +1,7 @@
 // Router.tsx
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { router } from '@/App'; // Import your existing routes
+import { router } from '@/pages/router'; // Import your existing routes
 
 const AppRouter: React.FC = () => {
     return <RouterProvider router={router} />;
