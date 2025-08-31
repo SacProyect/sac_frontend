3b8f61cbc6e0e7136a16ec184556095ce9822982
VictorDevCode21 <victorenrique2002@gmail.com>
2025-08-30 21:43:59 -0400

    high/medium/low compliance in fiscalStats succesfully exporting to excel

--- src/components/fiscal-stats/page2/MediumCompliance.tsx
+++ src/components/fiscal-stats/page2/MediumCompliance.tsx
@@ -1,4 +1,5 @@
 import { getFiscalTaxpayerCompliance } from '@/components/utils/api/reportFunctions';
+import { exportExcel } from '@/components/utils/exportReports';
 import { useAuth } from '@/hooks/useAuth';
 import { ComplianceInterface, FiscalInfo } from '@/types/reports';
 import { Download, Users } from 'lucide-react'
@@ -150,10 +151,20 @@ function MediumCompliance({ fiscalData }: MediumComplianceProps) {
                         <Users className="w-4 h-4 text-yellow-500" />
                         Contribuyentes con Cumplimiento Medio (34-66%)
                     </div>
-                    <div className="pt-4 pr-4">
+                    <div className="flex pt-4 pr-4 space-x-2">
                         <button
                             onClick={() => downloadPDF("medio-cumplimiento-fiscal-table", "medio-cumplimiento-fiscal.pdf")}
-                            className="px-2 text-white bg-blue-600 border-blue-600 hover:bg-blue-700 h-7"
+                            className="flex items-center justify-center px-2 text-white bg-blue-600 border-blue-600 hover:bg-blue-700 h-7"
+                        >
+                            <Download className="w-3 h-3" />
+                        </button>
+
+                        <button
+                            onClick={() =>
+                                compliance &&
+                                exportExcel(compliance, "medio-cumplimiento-fiscal", fiscalData.fiscalName)
+                            }
+                            className="flex items-center justify-center px-2 text-white bg-green-600 hover:bg-green-700 h-7"
                         >
                             <Download className="w-3 h-3" />
                         </button>
